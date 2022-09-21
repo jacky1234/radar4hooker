772fcbbe7ac39a1dd7cdf4420c2b29cf7ef9800a
YC <yingci@pinduoduo.com>
2022-09-21 10:28:50 +0800

    Wed Sep 21 10:28:50 CST 2022 refactor

--- makeDex4linux.sh
+++ makeDex4linux.sh
@@ -2,22 +2,20 @@ rm -rf classes/
 mkdir classes
 find . -name "*.java" > sources.txt
 javac -encoding utf-8 -nowarn -Xlint:unchecked -Xlint:deprecation -classpath android.jar:android-support-v4.jar:nanohttpd-2.3.1.jar:fastjson-1.2.9.jar:org.apache.http.legacy.jar:okhttp-3.12.6.jar -d classes @sources.txt
-cp nanohttpd-2.3.1.jar classes/
-cp okhttp-3.12.6.jar classes/
-cp okio-1.15.0.jar classes/
-cp fastjson-1.2.9.jar classes/
-cd classes/
-jar xvf nanohttpd-2.3.1.jar
-jar xvf okhttp-3.12.6.jar
-jar xvf okio-1.15.0.jar
-jar xvf fastjson-1.2.9.jar
+
+cd classes/ || return
+jar xvf ../nanohttpd-2.3.1.jar
+jar xvf ../okhttp-3.12.6.jar
+jar xvf ../okio-1.15.0.jar
+jar xvf ../fastjson-1.2.9.jar
 rm -rf *.jar
 jar cvf merge.jar .
 cd ..
 java -jar jarjar-1.3.jar process rule.txt classes/merge.jar classes/xradar.jar
 rm classes/merge.jar
 #替换你本地的dx路径
-alias dx=$ANDROID_HOME/build-tools/30.0.3/dx
+alias dx="$ANDROID_HOME"/build-tools/30.0.3/dx
 dx --dex --output=classes/radar.dex classes/xradar.jar
+unalias dx
 echo "xradar.jar 用于你的爬虫工程."
 echo "radar.dex用于替换hooker根目录下的radar.dex"
